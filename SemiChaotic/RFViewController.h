e350e4df78f39079b66a5d7f006543d1decf5606
bryce redd <bryce@i.tv>
2013-02-19 18:19:00 +0100

    Initial Commit

---
+++ SemiChaotic/RFViewController.h
@@ -0,0 +1,13 @@
+//
+//  RFViewController.h
+//  SemiChaotic
+//
+//  Created by Bryce Redd on 2/19/13.
+//  Copyright (c) 2013 Bryce Redd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RFViewController : UIViewController
+
+@end
---
+++ SemiChaotic/RFViewController.m
@@ -0,0 +1,29 @@
+//
+//  RFViewController.m
+//  SemiChaotic
+//
+//  Created by Bryce Redd on 2/19/13.
+//  Copyright (c) 2013 Bryce Redd. All rights reserved.
+//
+
+#import "RFViewController.h"
+
+@interface RFViewController ()
+
+@end
+
+@implementation RFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
